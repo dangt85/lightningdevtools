5cef577c398add4884924a594c4c6c09020220b7
Daniel Gonzalez <dangt85@gmail.com>
2017-06-01 18:32:56 -0400

    creating tabs service

---
+++ src/app/metadata/mock-metadata-tabs.ts
@@ -0,0 +1,42 @@
+export const tabs = [
+{
+    name: 'Metadata Diff',
+},
+{
+    name: 'Package Creator',
+    menuItems: [
+        {
+            name: 'Generate package.xml'
+        },
+        {
+            name: 'Generate Force.com IDE Project'
+        },
+        {
+            name: 'Generate SFDX Workspace'
+        }
+    ]
+},
+{
+    name: 'Deploy Tools',
+    menuItems: [
+        {
+            name: 'Simple Deploy'
+        },
+        {
+            name: 'Validate Package'
+        },
+        {
+            name: 'Deploy Package'
+        },
+        {
+            name: 'Schedule Package Deploy'
+        },
+        {
+            name: 'Create SF Deploy Button'
+        },
+        {
+            name: 'Create Heroku Deploy Button'
+        }
+    ]
+}
+];
---
+++ src/app/shared/app-tab.ts
@@ -0,0 +1,8 @@
+export interface AppTab {
+    name: string;
+    menuItems: MenuItem[];
+}
+
+export interface MenuItem {
+    name: string;
+}
