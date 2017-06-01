942595a4fc85b29d33a4e7595bd11bf5916a2f3c
Daniel Gonzalez <dangt85@gmail.com>
2017-06-01 18:46:04 -0400

    cleanup

--- src/app/metadata/mock-metadata-tabs.ts
+++ src/app/metadata/mock-metadata-tabs.ts
@@ -1,42 +1,16 @@
 export const tabs = [
-{
-    name: 'Metadata Diff',
-},
-{
-    name: 'Package Creator',
-    menuItems: [
-        {
-            name: 'Generate package.xml'
-        },
-        {
-            name: 'Generate Force.com IDE Project'
-        },
-        {
-            name: 'Generate SFDX Workspace'
-        }
-    ]
-},
-{
-    name: 'Deploy Tools',
-    menuItems: [
-        {
-            name: 'Simple Deploy'
-        },
-        {
-            name: 'Validate Package'
-        },
-        {
-            name: 'Deploy Package'
-        },
-        {
-            name: 'Schedule Package Deploy'
-        },
-        {
-            name: 'Create SF Deploy Button'
-        },
-        {
-            name: 'Create Heroku Deploy Button'
-        }
-    ]
-}
+    { name: 'Metadata Diff' },
+    { name: 'Package Creator', menuItems: [
+        { name: 'Generate package.xml' },
+        { name: 'Generate Force.com IDE Project' },
+        { name: 'Generate SFDX Workspace' }
+    ] },
+    { name: 'Deploy Tools', menuItems: [
+        { name: 'Simple Deploy' },
+        { name: 'Validate Package' },
+        { name: 'Deploy Package' },
+        { name: 'Schedule Package Deploy' },
+        { name: 'Create SF Deploy Button' },
+        { name: 'Create Heroku Deploy Button' }
+    ] }
 ];
