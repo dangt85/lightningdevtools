5222fc28839989eb37e7f0f5342b50240f952478
Daniel Gonzalez <dangt85@gmail.com>
2017-06-04 21:39:46 -0400

    successful connexion and service callout

--- src/app/metadata/metadata.component.ts
+++ src/app/metadata/metadata.component.ts
@@ -18,8 +18,6 @@ export class MetadataComponent implements OnInit {
   selectedTab: AppTab;
   selectedItem: MenuItem;
 
-  contacts: Array<any>;
-
   org1: any;
   org2: any;
 
@@ -32,6 +30,30 @@ export class MetadataComponent implements OnInit {
     });
   }
 
+  connect(org: string) {
+    let oauth = OAuth.createInstance("3MVG9FS3IyroMOh5pLo6sS_qz99RhYNFO7hVdpQ_ZaA8qn6pm8drQlAzFnTOSM_RmDzbgsgST90xNiv.4HP8o",
+      "https://cwc-dgtraining--sfdx.cs9.my.salesforce.com",
+      "http://localhost:4200/assets/oauthcallback.html");
+
+    oauth.login().then((oauthData) => {
+      // login was successful, save oauthData
+      if(org === 'org1')
+        this.org1 = oauthData;
+      else if(org === 'org2')
+        this.org2 = oauthData;
+      
+      console.log(oauthData);
+      // uses connection instance for for future reference
+      DataService.createInstance(oauthData, {proxyURL: "https://dev-cors-proxy.herokuapp.com/"}, org);
+    }).then(() => {
+      let service = DataService.getInstance(org);
+      service.resources()
+        .then(response => {
+          console.log(response);
+        });
+    });
+  }
+
   findAll() {
     let oauth = OAuth.createInstance("3MVG9FS3IyroMOh5pLo6sS_qz99RhYNFO7hVdpQ_ZaA8qn6pm8drQlAzFnTOSM_RmDzbgsgST90xNiv.4HP8o",
       "https://cwc-dgtraining--sfdx.cs9.my.salesforce.com",
@@ -41,7 +63,7 @@ export class MetadataComponent implements OnInit {
     }).then(() => {
       this.metadataService.findAll()
         .then(data => {
-          this.contacts = data;
+          // this.contacts = data;
         })
         .catch(err => alert(err));
     });
