74cbf279c7b7cf30b78c68476011b44001fdc766
Daniel Gonzalez <dangt85@gmail.com>
2017-06-06 14:16:19 -0400

    adds icon in tab object - WIP add to svg

--- src/app/data/mock-data-tabs.ts
+++ src/app/data/mock-data-tabs.ts
@@ -1,8 +1,8 @@
 import { AppTab, MenuItem } from "../shared/app-tab";
 
 export const tabs = [
-    new AppTab('Data Diff', 'Select sObjects and compare data between two instances.'),
-    new AppTab('Copy Data', 'Select sObjects and copy data from a source instance into a target instance.'),
-    new AppTab('Mock Data Generator', 'Select an sObject from an org and generate mock data for this sObject. Then choose to export as .csv or load the data into the org.'),
-    new AppTab('Test Data Generator', 'Select an sObject from an org and generate a static resource with a .csv file with the data for this sObject to be used in an Apex Test class.'),
+    new AppTab('Data Diff', 'Select sObjects and compare data between two instances.', 'relationship'),
+    new AppTab('Copy Data', 'Select sObjects and copy data from a source instance into a target instance.', 'product_item_transaction'),
+    new AppTab('Mock Data Generator', 'Select an sObject from an org and generate mock data for this sObject. Then choose to export as .csv or load the data into the org.', 'process'),
+    new AppTab('Test Data Generator', 'Select an sObject from an org and generate a static resource with a .csv file with the data for this sObject to be used in an Apex Test class.', 'poll'),
 ];
--- src/app/metadata/mock-metadata-tabs.ts
+++ src/app/metadata/mock-metadata-tabs.ts
@@ -1,13 +1,13 @@
 import { AppTab, MenuItem } from "../shared/app-tab";
 
 export const tabs = [
-    new AppTab('Metadata Diff', 'Select and compare metadata between two instances.'),
-    new AppTab('Package Creator', 'Select metadata from one instance and export it.', [
+    new AppTab('Metadata Diff', 'Select and compare metadata between two instances.', 'drafts'),
+    new AppTab('Package Creator', 'Select metadata from one instance and export it.', 'product', [
         new MenuItem('Generate package.xml'),
         new MenuItem('Generate Force.com IDE Project'),
         new MenuItem('Generate SFDX Workspace')
     ]),
-    new AppTab('Deploy Tools', 'Plan, validate or deploy code.', [
+    new AppTab('Deploy Tools', 'Plan, validate or deploy code.', 'product_request', [
         new MenuItem('Simple Deploy'),
         new MenuItem('Validate Package'),
         new MenuItem('Deploy Package'),
